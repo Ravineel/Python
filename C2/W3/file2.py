0d77bd215577d1c5946a9325b42e193c603dad9e
Ravineel <rajatsinghi89@gmail.com>
2019-09-30 22:38:16 +0530

    file2

---
+++ C2/W3/file2.py
@@ -0,0 +1,15 @@
+# Use the file name mbox-short.txt as the file name
+fname = input("Enter file name: ")
+fh = open(fname)
+avg = 0.0
+count = 0
+for line in fh:
+     
+    if not line.startswith("X-DSPAM-Confidence:") : 
+        continue
+    count = count + 1
+    a = line.find(' ')
+    b = line.find('\n',a)
+    avg = avg + float(line[a+1:b])
+print('Average spam confidence:',avg/count)
+
