e8c6c8a31086781abdaead6e9a2bed37ff8ac2ff
Ravineel <rajatsinghi89@gmail.com>
2019-10-01 00:38:01 +0530

    list1

---
+++ C2/w4/list1.py
@@ -0,0 +1,11 @@
+#file name = romeo.txt 
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    for x in line.split():
+        if x in lst:
+        	continue
+        lst.append(x)
+lst.sort()
+print(lst)
