3f21c4b11920e1dc9f315a6b5f38828a9a205847
Ravineel <rajatsinghi89@gmail.com>
2019-09-30 22:27:02 +0530

    file1

---
+++ C2/W3/file1.py
@@ -0,0 +1,10 @@
+# Use words.txt as the file name
+fname = input("Enter file name: ")
+
+try:
+    fh = open(fname)
+except:
+    print('wrong file name')
+
+for line in fh:
+    print(line.rstrip().upper())
