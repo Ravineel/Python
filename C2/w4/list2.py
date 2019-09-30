592abf0dad208af4ab8851a9ffd2d4c26c675546
Ravineel <rajatsinghi89@gmail.com>
2019-10-01 01:03:24 +0530

    list2

---
+++ C2/w4/list2.py
@@ -0,0 +1,19 @@
+#file name = mbox-short.txt
+fname = input("Enter file name: ")
+
+fh = open(fname)
+word  = list()
+count = 0
+for line in fh:
+    line.rstrip()
+    if line =='':continue
+    if not line.startswith("From:"): continue
+
+    word = line.split()
+    count = count + 1
+    print(word[1])
+
+
+
+print('There were',count,'lines in the file with From as the first word')
+
